6f8987229fb32de5bf2558a719f12b9394c1c4a7
Dat Nguyen <datnpt2004@gmail.com>
2025-02-07 17:35:44 +0700

    Update URLs

--- src/main/java/com/DatNguyen/ImageGenerator/ImageGeneratorApplication.java
+++ src/main/java/com/DatNguyen/ImageGenerator/ImageGeneratorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ImageGeneratorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ImageGeneratorApplication.class, args);
 	}
-
 }
