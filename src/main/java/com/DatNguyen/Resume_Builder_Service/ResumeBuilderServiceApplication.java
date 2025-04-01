3101feb1959010b4abcec64354fc55c83821c904
Dat Nguyen <datnpt2004@gmail.com>
2025-04-01 16:35:55 +0700

    Refactor: Add Swagger and Refactor code style

--- src/main/java/com/DatNguyen/Resume_Builder_Service/ResumeBuilderServiceApplication.java
+++ src/main/java/com/DatNguyen/Resume_Builder_Service/ResumeBuilderServiceApplication.java
@@ -2,9 +2,12 @@ package com.DatNguyen.Resume_Builder_Service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration.class
+})
 @EnableReactiveMongoRepositories(basePackages = "com.DatNguyen.Resume_Builder_Service.Repository")
 public class ResumeBuilderServiceApplication {
 
